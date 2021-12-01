b84a770420a335247e245d7757578fd92f04e547
eddy <2578052882@qq.com>
2021-12-01 11:25:57 +0800

    2021-12-01

---
+++ leetcode/editor/cn/[34]在排序数组中查找元素的第一个和最后一个位置.py
@@ -0,0 +1,66 @@
+# 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。 
+# 
+#  如果数组中不存在目标值 target，返回 [-1, -1]。 
+# 
+#  进阶： 
+# 
+#  
+#  你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？ 
+#  
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  
+# 输入：nums = [5,7,7,8,8,10], target = 8
+# 输出：[3,4] 
+# 
+#  示例 2： 
+# 
+#  
+# 输入：nums = [5,7,7,8,8,10], target = 6
+# 输出：[-1,-1] 
+# 
+#  示例 3： 
+# 
+#  
+# 输入：nums = [], target = 0
+# 输出：[-1,-1] 
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  0 <= nums.length <= 10⁵ 
+#  -10⁹ <= nums[i] <= 10⁹ 
+#  nums 是一个非递减数组 
+#  -10⁹ <= target <= 10⁹ 
+#  
+#  Related Topics 数组 二分查找 👍 1317 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (right - left) // 2 + left
+            if nums[mid] == target:
+                left_m, right_m = mid, mid
+                while left_m - 1 >= 0 and nums[left_m - 1] == target:
+                    left_m -= 1
+                while right_m + 1 < len(nums) and nums[right_m + 1] == target:
+                    right_m += 1
+                return [left_m, right_m]
+            if nums[mid] < target:
+                left = mid + 1
+            if nums[mid] > target:
+                right = mid - 1
+
+        return [-1, -1]
+# leetcode submit region end(Prohibit modification and deletion)
