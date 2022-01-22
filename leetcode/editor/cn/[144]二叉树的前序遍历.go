43e925d614a2bd1b33226fb17aa555c24fc66fb7
eddy <2578052882@qq.com>
2022-01-22 16:02:21 +0800

    :memo:Go语言算法

---
+++ leetcode/editor/cn/[144]二叉树的前序遍历.go
@@ -0,0 +1,78 @@
+//给你二叉树的根节点 root ，返回它节点值的 前序 遍历。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,null,2,3]
+//输出：[1,2,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1]
+//输出：[1]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：root = [1,2]
+//输出：[1,2]
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：root = [1,null,2]
+//输出：[1,2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 719 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+package main
+
+// TreeNode Definition for a binary tree node.
+//type TreeNode struct {
+//	Val   int
+//	Left  *TreeNode
+//	Right *TreeNode
+//}
+
+func preorderTraversal(root *TreeNode) (res []int) {
+	var traversal func(node *TreeNode)
+	traversal = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		res = append(res, node.Val)
+		traversal(node.Left)
+		traversal(node.Right)
+	}
+	traversal(root)
+	return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/cn/[94]二叉树的中序遍历.go
@@ -0,0 +1,78 @@
+//给定一个二叉树的根节点 root ，返回它的 中序 遍历。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1]
+//输出：[1]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：root = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：root = [1,null,2]
+//输出：[1,2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1244 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+func inorderTraversal(root *TreeNode) (res[]int) {
+	var traversal func(node *TreeNode)
+	traversal = func(node *TreeNode) {
+		if node == nil{
+			return
+		}
+		traversal(node.Left)
+		res = append(res,node.Val)
+		traversal(node.Right)
+	}
+	traversal(root)
+	return res
+}
+//leetcode submit region end(Prohibit modification and deletion)
